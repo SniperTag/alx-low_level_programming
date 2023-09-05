731d7ecc0a4198f4a0d623576314c08f550ed472
SniperTag <122808471+SniperTag@users.noreply.github.com>
2023-09-05 06:17:14 +0100

    Add files via upload
    libraries, argue and malloc done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - function to print all argument it recieve
+* @argc: first parameter
+* @argv: array pointer
+* Return: Always 0 0n success
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
